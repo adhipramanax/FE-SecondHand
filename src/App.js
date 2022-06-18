b696e996d309313aceda8517ea2c1f08ba49ae02
PANJI ADHIPRAMANA <panjiadhipramana@gmail.com>
2022-06-18 22:01:06 +0800

    [Panji] Make login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
 
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={""} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={""} />
         <Route path="/my-account" element={""} />
         <Route path="/store" element={""} />
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginExample = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+
+        </>
     );
 }
 
