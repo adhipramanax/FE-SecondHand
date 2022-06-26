224f850de252dcf090c57d5b805463c256966400
PANJI ADHIPRAMANA <panjiadhipramana@gmail.com>
2022-06-26 16:32:36 +0800

    [Panji] Make my account view

--- src/pages/seller/dashboard/WishList.jsx
+++ src/pages/seller/dashboard/WishList.jsx
@@ -1,33 +1,16 @@
-import React from 'react';
-import casioWOne from '../../../assets/images/Rectangle_24.png'
+import React from "react";
+import casioWOne from "../../../assets/images/Rectangle_24.png";
 
 const WishList = () => {
     return (
         <>
             <a href="/seller/offer-product">
-            <div class="col-6 col-md-4">
+                <div class="col-6 col-md-4">
                     <div class="card catalog-card">
-
                         <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
                         <div class="card-body">
                             <div class="card-body pt-2">
-                                <h5 className='catalog-title'>Jam Tangan Casio</h5>
-                                <p class="card-text text-muted">Aksesoris</p>
-                                <h5>Rp. 250.000</h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </a>
-            
-            <a href="/seller/offer-product">
-            <div class="col-6 col-md-4">
-                    <div class="card catalog-card">
-
-                        <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
-                        <div class="card-body">
-                            <div class="card-body pt-2">
-                                <h5 className='catalog-title'>Jam Tangan Casio</h5>
+                                <h5 className="catalog-title">Jam Tangan Casio</h5>
                                 <p class="card-text text-muted">Aksesoris</p>
                                 <h5>Rp. 250.000</h5>
                             </div>
@@ -37,13 +20,12 @@ const WishList = () => {
             </a>
 
             <a href="/seller/offer-product">
-            <div class="col-6 col-md-4">
+                <div class="col-6 col-md-4">
                     <div class="card catalog-card">
-
                         <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
                         <div class="card-body">
                             <div class="card-body pt-2">
-                                <h5 className='catalog-title'>Jam Tangan Casio</h5>
+                                <h5 className="catalog-title">Jam Tangan Casio</h5>
                                 <p class="card-text text-muted">Aksesoris</p>
                                 <h5>Rp. 250.000</h5>
                             </div>
@@ -53,13 +35,12 @@ const WishList = () => {
             </a>
 
             <a href="/seller/offer-product">
-            <div class="col-6 col-md-4">
+                <div class="col-6 col-md-4">
                     <div class="card catalog-card">
-
                         <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
                         <div class="card-body">
                             <div class="card-body pt-2">
-                                <h5 className='catalog-title'>Jam Tangan Casio</h5>
+                                <h5 className="catalog-title">Jam Tangan Casio</h5>
                                 <p class="card-text text-muted">Aksesoris</p>
                                 <h5>Rp. 250.000</h5>
                             </div>
@@ -69,13 +50,12 @@ const WishList = () => {
             </a>
 
             <a href="/seller/offer-product">
-            <div class="col-6 col-md-4">
+                <div class="col-6 col-md-4">
                     <div class="card catalog-card">
-
                         <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
                         <div class="card-body">
                             <div class="card-body pt-2">
-                                <h5 className='catalog-title'>Jam Tangan Casio</h5>
+                                <h5 className="catalog-title">Jam Tangan Casio</h5>
                                 <p class="card-text text-muted">Aksesoris</p>
                                 <h5>Rp. 250.000</h5>
                             </div>
@@ -85,13 +65,27 @@ const WishList = () => {
             </a>
 
             <a href="/seller/offer-product">
-            <div class="col-6 col-md-4">
+                <div class="col-6 col-md-4">
                     <div class="card catalog-card">
-
                         <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
                         <div class="card-body">
                             <div class="card-body pt-2">
-                                <h5 className='catalog-title'>Jam Tangan Casio</h5>
+                                <h5 className="catalog-title">Jam Tangan Casio</h5>
+                                <p class="card-text text-muted">Aksesoris</p>
+                                <h5>Rp. 250.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/seller/offer-product">
+                <div class="col-6 col-md-4">
+                    <div class="card catalog-card">
+                        <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
+                        <div class="card-body">
+                            <div class="card-body pt-2">
+                                <h5 className="catalog-title">Jam Tangan Casio</h5>
                                 <p class="card-text text-muted">Aksesoris</p>
                                 <h5>Rp. 250.000</h5>
                             </div>
@@ -101,6 +95,6 @@ const WishList = () => {
             </a>
         </>
     );
-}
+};
 
 export default WishList;
