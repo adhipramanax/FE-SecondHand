ecf31a1c56699dc750669fd868f3c7d7873916e6
HelenaHP <helenaaahp13@gmail.com>
2022-07-04 14:37:40 +0700

    [helena] Integration Wishlist my-account

--- src/pages/Buyyer/MyAccount/WishList.jsx
+++ src/pages/Buyyer/MyAccount/WishList.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import casioWOne from "../../../assets/images/Rectangle_24.png";
+import ListProduct from "../../../components/ListProductCard"
+import { getWishlistProduct } from "../../../services/productService";
 
 const Catalog = () => {
+    const [products, setProducts] = useState([]) 
+    
+    useEffect(() => {
+        getWishlistProduct().then((response) => setProducts(response.data.data));
+    }, []);
+
     return (
         <>
-            <div class="col-6 col-md-4">
+            <div class="container mt-4 mb-4">
+                <div class="row g-4">
+                    <ListProduct data={products} action={false} link="/seller/product/" />
+                </div>
+            </div>
+
+
+            {/* <div class="col-6 col-md-4">
                 <a href="/seller/detail-product" class="text-dark text-decoration-none">
                     <div class="card catalog-card">
                         <img src={casioWOne} class="card-img-top" alt="jam-tangan" />
@@ -45,7 +60,7 @@ const Catalog = () => {
                         </div>
                     </div>
                 </a>
-            </div>
+            </div> */}
         </>
     );
 };
