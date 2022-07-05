01cfbd1f5c8ff5c3e9e17b78ee556519dad2553a
dhefanza20 <dhefanza20@gmail.com>
2022-07-05 10:59:37 +0700

    [Dhefanza] Integration view create product

--- src/pages/seller/CreateProduct/InputForm.jsx
+++ src/pages/seller/CreateProduct/InputForm.jsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import backBtn from '../../../assets/images/fi_arrow-left.png';
+import React, { useEffect, useState } from "react";
+import backBtn from "../../../assets/images/fi_arrow-left.png";
+import Input from "../../../components/Input";
+import ActionButton from "../../../components/ActionButton";
 
 const InputForm = () => {
+    const [productName, setProductName] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [description, setDescription] = useState("");
+    const [productPhoto, setProductPhoto] = useState("");
+
+    const handleProductName = (e) => {
+        setProductName(e.target.value);
+    };
+
+    const handlePrice = (e) => {
+        setPrice(e.target.value);
+    };
+
+    const handleCategory = (e) => {
+        setCategory(e.target.value);
+    };
+
+    const handleDescription = (e) => {
+        setDescription(e.target.value);
+    };
+
+    const handleProductPhoto = (e) => {
+        setProductPhoto(e.target.value);
+    };
+
+    const handlePreview = (e) => {};
+
+    const handleSubmit = (e) => {
+        const data = {
+            productName,
+            price,
+            category,
+            description,
+            productPhoto,
+        };
+    };
     return (
         <>
             <section class="input-form">
                 <div class="container">
                     <div class="row d-flex flex-wrap align-content-center justify-content-center">
                         <div class="col-1 col-lg-1 mt-4">
-                            <a href="/seller/store#catalog"><img src={backBtn} alt="back-button" /></a>
+                            <a href="/seller/store#catalog">
+                                <img src={backBtn} alt="back-button" />
+                            </a>
                         </div>
-                        
+
                         <div class="col-11 mt-4">
                             <h5 class="text-center fw-bold">Lengkapi Detail Product</h5>
                         </div>
                         <div class="col-12 col-lg-11 mt-4">
-                            <p class="mb-1">Nama Produk</p>
-                            <input class="input-style mb-3" type="text" placeholder='Nama Produk' />
-                            <p class="mb-1">Harga Produk</p>
-                            <input class="input-style mb-3" type="number" name="price-product" id="price-product" placeholder='Rp. 0,00' />
+                            <Input
+                                label="Nama Produk"
+                                type="text"
+                                name="product-name"
+                                id="product-name"
+                                width="100%"
+                                placeholder="Nama Produk"
+                                value={productName}
+                                onChange={(value) => handleProductName(value)}
+                            />
+
+                            <Input label="Harga" type="number" name="product-price" id="product-price" width="100%" placeholder="Rp. 0,00" value={price} onChange={(value) => handlePrice(value)} />
+
                             <p class="mb-1">Kategori</p>
                             <select class="input-style mb-3" name="category-product" id="category-product">
                                 <option value="1">Semua</option>
@@ -27,24 +77,35 @@ const InputForm = () => {
                                 <option value="4">Elektronik</option>
                                 <option value="5">Kesehatan</option>
                             </select>
-                            <p class="mb-1">Deskripsi</p>
-                            <input class="input-style mb-3" type="text" placeholder='Contoh: Jalan Ikan Hiu 33' />
-                            <p class="mb-1">Foto Produk</p>
-                            <input type="file" name="img-product" id="img-product"/>
+
+                            <Input
+                                label="Deskripsi"
+                                type="text"
+                                name="description"
+                                id="description"
+                                width="100%"
+                                placeholder="Contoh: Jalan Ikan Hiu 33"
+                                value={description}
+                                onChange={(value) => handleDescription(value)}
+                            />
+
+                            <Input label="Foto Produk" type="file" name="product-photo" id="product-photo" width="100%" value={productPhoto} onChange={(value) => handleProductPhoto(value)} />
+
                             <div class="row mt-4 mb-4">
                                 <div class="col-6">
-                                    <button class="btn btn-color-white px-2 w-100 post-button">Preview</button>
+                                    <ActionButton color="#000000" bg="#FFFFFF" text="Preview" width="100%" onClick={(e) => handlePreview(e)} />
                                 </div>
+
                                 <div class="col-6">
-                                    <button class="btn btn-color-purple px-2 w-100 post-button">Terbitkan</button>
+                                    <ActionButton color="#FFFFFF" bg="#4B1979" text="Terbitkan" width="100%" onClick={(e) => handleSubmit(e)} />
                                 </div>
-                            </div>  
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
         </>
     );
-}
+};
 
 export default InputForm;
